54e147ca2bb7a702a4ebe3943915f22738f1c1e6
Ariaden-Bai <bairuoqi@yeah.net>
2021-05-28 15:31:28 +0800

    webpack can build

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import LOGO from './pedro.svg';
 
 export const App = () => {
     return <>
-    <h1>MUSCI frontend - {process.env.NODE_ENV} {process.env.name}</h1>
+    <h1>MUSCI Frontend - {process.env.NODE_ENV} {process.env.name}</h1>
     <img src={IMAGE} alt="React Logo" width="300" />
     <img src={LOGO} alt="React Logo" height="500" />
     </>

7aa2edfc1cfced845d9f76b0c00857cfcfcd487d
Ariaden-Bai <bairuoqi@yeah.net>
2021-05-28 16:52:04 +0800

    added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css';
 import IMAGE from './react-logo.png';
 import LOGO from './pedro.svg';
+import { CounterButton } from './CounterButton'
 
 export const App = () => {
     return <>
-    <h1>MUSCI Frontend - {process.env.NODE_ENV} {process.env.name}</h1>
+    <h1>MUSCI Home Page - {process.env.NODE_ENV} {process.env.name}</h1>
     <img src={IMAGE} alt="React Logo" width="300" />
     <img src={LOGO} alt="React Logo" height="500" />
+    <CounterButton />
     </>
 }

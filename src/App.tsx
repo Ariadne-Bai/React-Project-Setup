d87d73a53889a9b39c8cd51cfd305cd0297eb340
Ariaden-Bai <bairuoqi@yeah.net>
2021-05-28 17:23:00 +0800

    added eslint for linting and prettier for formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import './styles.css';
-import IMAGE from './react-logo.png';
-import LOGO from './pedro.svg';
+import './styles.css'
+import IMAGE from './react-logo.png'
+import LOGO from './pedro.svg'
 import { CounterButton } from './CounterButton'
 
 export const App = () => {
-    return <>
-    <h1>MUSCI Home Page - {process.env.NODE_ENV} {process.env.name}</h1>
-    <img src={IMAGE} alt="React Logo" width="300" />
-    <img src={LOGO} alt="React Logo" height="500" />
-    <CounterButton />
-    </>
+    return (
+        <>
+            <h1>
+                MUSCI Home Page - {process.env.NODE_ENV} {process.env.name}
+            </h1>
+            <img src={IMAGE} alt="React Logo" width="300" />
+            <img src={LOGO} alt="React Logo" height="500" />
+            <CounterButton />
+        </>
+    )
 }
--- src/CounterButton.tsx
+++ src/CounterButton.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 
 export const CounterButton = () => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
     return (
         <div>
-          <button onClick={() => setCount(c => c + 1)}>Count {count}</button>
+            <button onClick={() => setCount((c) => c + 1)}>
+                Count {count}
+            </button>
         </div>
     )
 }

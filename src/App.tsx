94ec04d52623336d37c2c53102af8d65e6fe7063
Ariaden-Bai <bairuoqi@yeah.net>
2021-05-28 15:05:31 +0800

    webpack and babel is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css';
+import IMAGE from './react-logo.png';
+import LOGO from './pedro.svg';
+
 export const App = () => {
-    return <h1>MUSCI frontend setup</h1>
+    return <>
+    <h1>MUSCI frontend - {process.env.NODE_ENV} {process.env.name}</h1>
+    <img src={IMAGE} alt="React Logo" width="300" />
+    <img src={LOGO} alt="React Logo" height="500" />
+    </>
 }
